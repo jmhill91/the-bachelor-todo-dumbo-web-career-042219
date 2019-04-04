2232e9044efb6e7a3c06b382a2ca2c3f0258d2e7
John Hill <john.m.hill91@gmail.com>
2019-04-04 15:38:59 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,8 @@ def get_first_name_of_season_winner(data, season)
     if cont["status"] == "Winner"
       winner = cont["name"]
     end
-    winner
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
